12231353a067b6cd363454ee351fdc86ed1fa276
Lucas Furriel <lrodrigues@coachit.com.br>
2023-02-02 18:45:14 -0300

    Tratando exception

--- application/Program.java
+++ application/Program.java
@@ -1,9 +1,11 @@
 package application;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
@@ -13,18 +15,25 @@ public class Program {
         Scanner sc = new Scanner(System.in);
 
         while (true) {
+            try {
+                UI.clearScreen();
 
-            UI.printBoard(match.getPieces()); //Printa o Tabuleiro
+                UI.printBoard(match.getPieces()); //Printa o Tabuleiro
 
-            //Le a posição de origem e destino
-            System.out.println();
-            System.out.println("Source: ");
-            ChessPosition source = UI.readChessPosition(sc);
-            System.out.println();
-            System.out.println("Target: ");
-            ChessPosition target = UI.readChessPosition(sc);
+                //Le a posição de origem e destino
+                System.out.println();
+                System.out.println("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+                System.out.println();
+                System.out.println("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
 
-            ChessPiece captured = match.performChessMove(source, target); //Executa a jogada
+                ChessPiece captured = match.performChessMove(source, target); //Executa a jogada
+            } catch (ChessException | InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+
         }
     }
 }
