c14d86fa00ccecc9d17e05ecf7c8c117a3869e20
Lucas Furriel <lrodrigues@coachit.com.br>
2023-02-02 16:59:10 -0300

    Adicionando cores as peças no terminal

--- chess/ChessMatch.java
+++ chess/ChessMatch.java
@@ -1,11 +1,11 @@
 package chess;
 
 import board.Board;
-import board.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
 public class ChessMatch {
+
     private Board board;
 
     public ChessMatch() {
@@ -14,20 +14,17 @@ public class ChessMatch {
     }
 
     public ChessPiece[][] getPieces() {
-        int i, j;
-        ChessPiece [][] mat = new ChessPiece[board.getRow()][board.getColumn()];
-        for (i = 0; i < board.getRow(); i++) {
-            for (j = 0; j < board.getColumn(); j++) {
+        ChessPiece[][] mat = new ChessPiece[board.getRow()][board.getColumn()];
+        for (int i=0; i<board.getRow(); i++) {
+            for (int j=0; j<board.getColumn(); j++) {
                 mat[i][j] = (ChessPiece) board.piece(i, j);
             }
         }
-
         return mat;
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
-
     }
 
     private void initialSetup() {
